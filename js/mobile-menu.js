3b2afc835749965fe62eb899a42ad77888a96f5a
shkurkoka <shkurkoka@gmail.com>
2023-03-07 13:18:27 +0200

    Mobile menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,12 @@
+(() => {
+  const mobileMenu = document.querySelector("[mobile-menu]");
+  const openMenuBtn = document.querySelector("[mobile-btn-open-menu]");
+  const closeMenuBtn = document.querySelector("[mobile-btn-close-menu]");
+    
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+  
+  const toggleMenu = () => {
+    
+  };
+})();
