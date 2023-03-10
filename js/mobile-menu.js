a87bc7587959145355ee24701c2524e45df994b3
shkurkoka <shkurkoka@gmail.com>
2023-03-10 19:23:58 +0200

    tablet update

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,12 +1,14 @@
 (() => {
-  const mobileMenu = document.querySelector("[mobile-menu]");
-  const openMenuBtn = document.querySelector("[mobile-btn-open-menu]");
-  const closeMenuBtn = document.querySelector("[mobile-btn-close-menu]");
-    
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-  
-  const toggleMenu = () => {
-    
+  const refs = {
+    openMobileBtn: document.querySelector("[mobile-btn-open-menu]"),
+    closeMobileBtn: document.querySelector("[mobile-btn-close-menu]"),
+    mobileMenu: document.querySelector("[mobile-menu]"),
   };
+
+  refs.openMobileBtn.addEventListener("click", toggleMobileMenu);
+  refs.closeMobileBtn.addEventListener("click", toggleMobileMenu);
+
+  function toggleMobileMenu() {
+    refs.mobileMenu.classList.toggle("is-hidden");
+  }
 })();
